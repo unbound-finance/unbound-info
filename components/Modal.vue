24bf743fd863fb927166e0ca98627de4a47bee9a
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-09-22 12:22:21 +0530

    fix: types node

--- components/Modal.vue
+++ components/Modal.vue
@@ -2,7 +2,7 @@
   <div>
     <button class="p-2" @click="isModalOpen = true">⚙️ Settings</button>
     <VueFinalModal v-model="isModalOpen" classes="modal-container" content-class="modal-content">
-        <div class="p-3 bg-blue-500 text-white font-medium rounded-t-lg">Settings</div>
+        <div class="p-3 bg-light-primary-500 text-white font-medium rounded-t-lg">Settings</div>
       <div class="grid grid-cols-3">
         <div class="col-span-1">left</div>
         <div class="col-span-2">right</div>
