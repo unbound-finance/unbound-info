244d790c341dfa97c103f505633eaacb6d5cd284
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-09-25 12:53:32 +0530

    chore: network select comp prune

--- components/NetworkSelectMenu.vue
+++ components/NetworkSelectMenu.vue
@@ -142,38 +142,7 @@
                 {{ option }}
               </span>
             </div>
-
-            <!--
-          Checkmark, only display for selected option.
-
-          Highlighted: "text-white", Not Highlighted: "text-light-primary"
-        -->
-            <!-- <span
-              class="absolute inset-y-0 right-0 flex items-center pr-4"
-              :class="
-                hoverState === option
-                  ? 'text-white'
-                  : 'text-light-primary dark:text-gray-100'
-              "
-              v-if="selected === option"
-            >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span> -->
-            <!-- End of Checkmark -->
           </li>
-          <!-- More items... -->
         </ul>
       </transition>
     </div>
