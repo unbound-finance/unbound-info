cf01ed0858c0ec63a7d8a400c2674064e9aa483b
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-09-25 13:30:57 +0530

    ft: DoubleLogo handle logo not found

--- components/DoubleLogo/index.vue
+++ components/DoubleLogo/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="dl__wrapper">
     <div class="dl__token0">
-      <img :src="token0logo" alt="token0" />
+      <img :src="token0" @error="onImageError" />
     </div>
     <div class="dl__token1">
-      <img :src="token1logo" alt="token1" />
+      <img :src="token1" @error="onImageError" />
     </div>
   </div>
 </template>
@@ -16,7 +16,7 @@ export default {
      * Token 0 Logo
      * @type String
      */
-    token0logo: {
+    token0: {
       type: String,
       required: true,
     },
@@ -24,46 +24,46 @@ export default {
      * Token 1 Logo
      * @type String
      */
-    token1logo: {
+    token1: {
       type: String,
       required: true,
+    },
+  },
+  methods: {
+    onImageError(event) {
+      event.target.src = '/ban.svg'
     },
   },
 }
 </script>
 
-<style lang="scss">
+<style>
 :root {
   --logo-size: 24px;
 }
-
 .dl__wrapper {
   position: relative;
   display: flex;
   width: calc(var(--logo-size) * 2);
 }
-
 .dl__token0 {
   display: flex;
   align-items: center;
   align-self: center;
 }
-
 .dl__token0 > img {
   width: var(--logo-size);
   height: var(--logo-size);
-  z-index: 2;
-  border-radius: 9999px;
+  border-radius: 100%;
 }
-
 .dl__token1 {
   position: absolute;
   left: calc(var(--logo-size) / 2);
 }
-
 .dl__token1 > img {
   width: var(--logo-size);
   height: var(--logo-size);
-  border-radius: 9999px;
+  z-index: 2;
+  border-radius: 100%;
 }
 </style>
